fcf27fcdc332af6e9fd431aef28175c57aabdbd1
wangyunpeng_bio <wangyunpeng_bio@qq.com>
2019-03-21 19:43:35 +0800

    Not optimized 003

---
+++ 003-longestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,12 @@
+class Solution:
+    def testUnique(self,s:str):
+        myset = set(s)
+        if len(myset) != len(s):
+            return False
+        return True
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        ans = 0
+        for i in range(len(s)):
+            for j in range(i+1,len(s)+1):
+                if self.testUnique(s[i:j]): ans = max(ans,j-i)
+        return ans 
