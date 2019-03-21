5d4ef2bc70203e921ad52ec9783aa6d2e7f36aa8
wangyunpeng_bio <wangyunpeng_bio@qq.com>
2019-03-21 10:16:34 +0800

    002-addTwoNumbers

---
+++ 002-addTwoNumbers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        result = ListNode(0)
+        r = result
+        carry = 0
+        while(l1 or l2):
+            a = l1.val if l1 else 0
+            b = l2.val if l2 else 0
+            s = carry + a + b # 当前位置的和
+            carry = s // 10 # 进几位
+            r.next = ListNode(s % 10)
+            r = r.next
+            if(l1!=None):l1=l1.next
+            if(l2!=None):l2=l2.next
+        if(carry>0):
+            r.next=ListNode(1)
+        return(result.next)
