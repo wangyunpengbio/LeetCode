e92dde5d950a016fe7bdb2d3aadcc874153d0034
wangyunpeng_bio <wangyunpeng_bio@qq.com>
2019-03-22 19:32:17 +0800

    update 004

--- 004-MedianofTwoSortedArrays.py
+++ 004-MedianofTwoSortedArrays.py
@@ -1,8 +1,6 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        numslen1 = len(nums1)
-        numslen2 = len(nums2)
-        numLeng = numslen1 + numslen2
         nums1.extend(nums2)
         nums1.sort()
+        numLeng = len(nums1)
         return((nums1[int(numLeng/2)] + nums1[int((numLeng-1)/2)])/2)
