26a58c6512ebc8981d3bcb95f64c3840d8980fa7
wangyunpeng_bio <wangyunpeng_bio@qq.com>
2019-03-22 19:48:14 +0800

    005-longestPalindromicSubstring

---
+++ 005-longestPalindromicSubstring.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        ans = 0
+        res = ""
+        for i in range(len(s)):
+            for j in range(i+1,len(s)+1):
+                if s[i:j][::-1] == s[i:j] and j-i > ans:
+                    ans = max(ans,j-i)
+                    res = s[i:j]
+        return res
+        
