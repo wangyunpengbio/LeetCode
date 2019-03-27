020fa08fe8d120ccf2c24961dfbe1a9779769ee6
wangyunpeng_bio <wangyunpeng_bio@qq.com>
2019-03-27 11:05:19 +0800

    015 update annotation

--- 015-v2-3Sum.py
+++ 015-v2-3Sum.py
@@ -16,10 +16,10 @@ class Solution:
                         res.append([nums[i],nums[l],nums[r]])
                         l +=1
                         r -=1
-                        # 后边的or条件是去除l的重复。
+                        # 后边的and条件是去除l的重复。
                         while l < r and nums[l] == nums[l-1]:
                             l += 1
-                        # 后边的and条件是去除l的重复。
+                        # 后边的and条件是去除r的重复。
                         while r > l and nums[r] == nums[r+1]:
                             r -= 1
                     elif s>0:
