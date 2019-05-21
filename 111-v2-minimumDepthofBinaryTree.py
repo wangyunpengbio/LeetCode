1613b03568d0ce4c139a1691e32a603d9c3dec96
wangyunpeng_bio <wangyunpeng_bio@qq.com>
2019-05-21 20:04:36 +0800

    111 annotation

--- 111-v2-minimumDepthofBinaryTree.py
+++ 111-v2-minimumDepthofBinaryTree.py
@@ -7,11 +7,12 @@
 
 class Solution:
     def minDepth(self, root: TreeNode) -> int:
+        # 类似于层次遍历 利用队列，队列中每个元素为(节点,层数)
         if root == None:
             return 0
         level = 1
         queue = [(root,1)]
-        while len(queue) != 0:
+        while len(queue) != 0: # 由于是二叉树，迟早会有叶子节点(无子节点)
             item,level = queue.pop(0)
             if item.left == None and item.right == None:
                 return level
