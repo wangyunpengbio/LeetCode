f51387960c0f299c9223eb54debdfb44161c671c
wangyunpeng_bio <wangyunpeng_bio@qq.com>
2019-06-20 20:42:00 +0800

    219 More

--- 219-v2-containsDuplicateII.py
+++ 219-v2-containsDuplicateII.py
@@ -6,3 +6,12 @@ class Solution:
             if nums[i] in nums[max(0,i-k):i] or nums[i] in nums[i+1:i+k+1]:
                 return True
         return False
+
+class Solution:
+    # 超时
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # 直接比较后k个元素拿出来比较即可
+        for i in range(len(nums)):
+            if nums[i] in nums[i+1:i+k+1]:
+                return True
+        return False
